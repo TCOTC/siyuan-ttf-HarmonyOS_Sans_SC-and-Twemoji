9aca13f0e403e4d8e231bec336a86b1419f66e6f
Jeffrey Chen <78434827+TCOTC@users.noreply.github.com>
2025-11-01 08:52:20 +0800

    v1.2.4
    导出 PDF 需要应用字体 https://github.com/TCOTC/siyuan-ttf-HarmonyOS_Sans_SC-and-Twemoji/issues/2

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
 module.exports = class F extends require('siyuan').Plugin {
     t = 'HarmonyOS Sans and Twemoji: ';
+    s = document.createElement('style');
 
     onload() {
-        setTimeout(() => {
-            if (!document.fonts || typeof document.fonts.load !== 'function') return;
-            try {
-                // 预加载 HarmonyOS Sans 的四种变体
-                document.fonts.load('400 16px "HarmonyOS Sans"', '1');
-                document.fonts.load('700 16px "HarmonyOS Sans"', '2');
-                document.fonts.load('italic 400 16px "HarmonyOS Sans"', '3');
-                document.fonts.load('italic 700 16px "HarmonyOS Sans"', '4');
-            } catch (_) {}
-            console.log(this.t + 'loaded');
-        }, 0);
+        fetch('../plugins/' + this.name + '/style.css')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`Failed to load CSS file`);
+                }
+                return response.text();
+            })
+            .then(css => {
+                this.s.id = 'snippetCSS-' + this.name; // id 以 snippet 开头的 style 会被添加到导出 PDF 中 https://github.com/siyuan-note/siyuan/commit/4318aa446369eaf4ea85982ba4919b5d47340552
+                this.s.textContent = css;
+                document.head.appendChild(this.s);
+
+                setTimeout(() => {
+                    if (!document.fonts || typeof document.fonts.load !== 'function') return;
+                    try {
+                        // 预加载 HarmonyOS Sans 的四种变体
+                        document.fonts.load('400 16px "HarmonyOS Sans"', '1');
+                        document.fonts.load('700 16px "HarmonyOS Sans"', '2');
+                        document.fonts.load('italic 400 16px "HarmonyOS Sans"', '3');
+                        document.fonts.load('italic 700 16px "HarmonyOS Sans"', '4');
+                    } catch (_) {}
+                    console.log(this.t + 'loaded');
+                }, 0);
+            })
+            .catch(error => {
+                console.error(this.t + error);
+            });
     }
 
     onunload() {
+        this.s?.remove();
         console.log(this.t + 'unloaded');
     }
 
     uninstall() {
+        this.s?.remove();
         console.log(this.t + 'uninstall');
     }
 }
